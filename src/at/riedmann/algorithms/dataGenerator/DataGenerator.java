9075a27d5a32cb55cfe9b4d9bc8b4fa5b6e05030
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2024-11-28 10:29:29 +0100

    Finished data generator and tests

--- src/at/riedmann/algorithms/dataGenerator/DataGenerator.java
+++ src/at/riedmann/algorithms/dataGenerator/DataGenerator.java
@@ -1,11 +1,11 @@
-package at.riedmann.algorithms;
+package at.riedmann.algorithms.dataGenerator;
 
 import java.util.Random;
 
 public class DataGenerator {
     public static int[] generateDataArray(int size){
         Random random = new Random();
-        int[] data = {};
+        int[] data = new int[size];
         for (int i = 0; i < size; i++) {
             data[i] = random.nextInt();
         }
@@ -14,14 +14,14 @@ public class DataGenerator {
 
     public static int[] generateDataArray(int size, int min, int max){
         Random random = new Random();
-        int[] data = {};
+        int[] data = new int[size];
         for (int i = 0; i < size; i++) {
             data[i] = min + random.nextInt(max-min);
         }
         return data;
     }
 
-    public void printArray(int[] data){
+    public static void printArray(int[] data){
         for (int i = 0; i < data.length; i++) {
             System.out.println(data[i]);
         }

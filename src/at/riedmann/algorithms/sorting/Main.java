46d3f4b5e12bde513e35781f4ea11602ffe6578a
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2024-12-05 12:19:42 +0100

    SelectionSort W.I.P.

--- src/at/riedmann/algorithms/sorting/Main.java
+++ src/at/riedmann/algorithms/sorting/Main.java
@@ -4,10 +4,11 @@ import at.riedmann.algorithms.dataGenerator.DataGenerator;
 
 public class Main {
     public static void main(String[] args) {
-        int[] data = DataGenerator.generateDataArray(5, 0, 10);
+        int[] data = DataGenerator.generateDataArray(5, -10, 10);
         DataGenerator.printArray(data);
         BubbleSort bs = new BubbleSort();
-        bs.sort(data);
+        SelectionSort ss = new SelectionSort();
+        ss.sort(data);
         System.out.println("Sorted:");
         DataGenerator.printArray(data);
     }
---
+++ src/at/riedmann/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+package at.riedmann.algorithms.sorting;
+
+public class SelectionSort implements Sorter {
+    /*
+     * SelectionSort wählt immer die kleinste Zahl und fügt diese in die neue Liste hinzu
+     * W.I.P.
+     */
+
+    @Override
+    public int[] sort(int[] data) {
+        int[] sortedList = data; 
+        for (int i = 0; i < data.length - 1; i++) {
+            int leastIndex = i; 
+            for (int j = i + 1; j < sortedList.length; j++) {
+                if (sortedList[j] < sortedList[leastIndex]) {
+                    leastIndex = j; 
+                }
+            }
+            if (leastIndex != i) {
+                int temp = sortedList[i];
+                sortedList[i] = sortedList[leastIndex];
+                sortedList[leastIndex] = temp;
+            }
+        }
+
+        return sortedList; 
+    }
+
+    @Override
+    public String getName() {
+        return "SelectionSort";
+    }
+
+}
